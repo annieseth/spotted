a94f5f5fa46b7c993c94275527618fd5b6ca0c4f
danielwang-personal <daniel867181@gmail.com>
2022-11-09 07:37:12 -0500

    changed input to text box and added notification

--- spotted/src/screens/EventRequestScreen.js
+++ spotted/src/screens/EventRequestScreen.js
@@ -10,6 +10,9 @@ const EventRequestScreen = ({ navigation }) => {
   const [time, setTime] = useState("15 minutes");
   const [location, setLocation] = useState("Scheller");
 
+  // used to pause runtime
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+
   const handlePress = async () => {
     console.log(time);
     console.log(location);
@@ -27,15 +30,26 @@ const EventRequestScreen = ({ navigation }) => {
     console.log("time IS " + time);
 
     /* This block is used for phones */
-    // Alert.alert(
-    //   "Request submitted!",
-    //   [
-    //     { text: "OK", onPress: () => console.log("OK Pressed") }
-    //   ]
-    // )
 
+    Alert.alert(
+      "Request submitted!",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    )
+    
+    /* Use this to mock an popup */
+    // wait 5 seconds
+    await sleep(2 * 1000); 
+    Alert.alert(
+      "Daniel has invited you to meet up!",
+      [
+        { text: "View", onPress: () =>navigation.navigate("Invites") },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    )
     /* This block is used for web development */
-    alert("Request submitted!")
+    // alert("Request submitted!")
     navigation.navigate("Home")
 
   };
@@ -48,14 +62,20 @@ const EventRequestScreen = ({ navigation }) => {
         <Text>
           When: 
         </Text>
-        <Picker
+        {/* scroll wheel */}
+        {/* <Picker
           selectedValue={time}
           onValueChange={(itemValue, itemIndex) =>
             setTime(itemValue)
           }>
           <Picker.Item label="15 minutes" value="15 minutes" />
           <Picker.Item label="30 minutes" value="30 minutes" />
-        </Picker> 
+        </Picker>  */}
+        <TextInput
+          style={styles.input}
+          onChangeText={setTime}
+          value={time}
+        />
       </View>
 
       {/* View for the Where Text input Form */}
@@ -64,7 +84,7 @@ const EventRequestScreen = ({ navigation }) => {
         >
           Where: 
         </Text>
-        <Picker
+        {/* <Picker
           selectedValue={location}
           onValueChange={(itemValue, itemIndex) =>
             setLocation(itemValue)
@@ -74,7 +94,12 @@ const EventRequestScreen = ({ navigation }) => {
           <Picker.Item label="Exhibition Hall" value="Exhibition Hall" />
           <Picker.Item label="Tech Square" value="Tech Square" />
           <Picker.Item label="Tech Green" value="Tech Green" />
-        </Picker>        
+        </Picker>  */}
+        <TextInput
+          style={styles.input}
+          onChangeText={setLocation}
+          value={location}
+        />       
       </View>
 
       <Button 
@@ -84,6 +109,15 @@ const EventRequestScreen = ({ navigation }) => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default EventRequestScreen;
   
