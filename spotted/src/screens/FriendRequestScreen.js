9616f337eb9d20b0afa3b57cb41d8d2ad20373df
RS <rushiji999@gmail.com>
2022-11-21 19:14:33 -0500

    added Search Bar to the FriendInvite Screen

--- spotted/src/screens/FriendRequestScreen.js
+++ spotted/src/screens/FriendRequestScreen.js
@@ -2,110 +2,62 @@ import * as React from 'react';
 import { Component } from 'react';
 import {useState} from 'react';
 import { Button, View, Text, Switch, TextInput, StyleSheet, Alert } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
 import {Auth, API, graphqlOperation} from 'aws-amplify';
 import { createEvent } from '../graphql/mutations';
+import { SearchBar } from 'react-native-elements';
   
 const FriendRequestScreen = ({ navigation }) => {
   const [time, setTime] = useState("15 minutes");
-  const [location, setLocation] = useState("Scheller");
+  
+  const [friends, setFriends] = useState(
+    [
+      {
+        id: 0,
+        name: 'John Smith',
+        activeSince: '1:53pM'
+      },
+      {
+        id: 1,
+        name: 'Granny Jones',
+        activeSince: '2:22PM'
+      },
+      {
+        id: 0,
+        name: 'Jack Hungry',
+        activeSince: '2:53PM'
+      },
+      {
+        id: 0,
+        name: 'Kindle Salt',
+        activeSince: '1:34PM'
+      },
+    ]
+  )
 
   // used to pause runtime
   const sleep = ms => new Promise(r => setTimeout(r, ms));
 
   const handlePress = async () => {
-    console.log(time);
-    console.log(location);
 
-    const user = await Auth.currentAuthenticatedUser();
-
-    Promise.resolve();
-    await API.graphql({ query: createEvent, variables: {input: {
-      inviteeUsername: "Dummy Name",
-      location: location,
-      meetTime: time,
-    }}, authMode: "AMAZON_COGNITO_USER_POOLS" });
-
-    console.log("Locations IS " + location);
-    console.log("time IS " + time);
-
-    /* This block is used for phones */
-
-    Alert.alert(
-      "Request submitted!",
-      [
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    )
-    
-    /* Use this to mock an popup */
-    // wait 5 seconds
-    await sleep(2 * 1000); 
-    Alert.alert(
-      "Daniel has invited you to meet up!",
-      [
-        { text: "View", onPress: () =>navigation.navigate("Invites") },
-        { text: "OK", onPress: () => console.log("OK Pressed") }
-      ]
-    )
-    /* This block is used for web development */
-    // alert("Request submitted!")
     navigation.navigate("Home")
 
   };
 
   return (
-    <View >  
-    <Text>Fill out the Information below to send the Invite</Text>
-      {/* View for the When Text input Form */}
-      <View >
-        <Text>
-          When: 
-        </Text>
-        {/* scroll wheel */}
-        {/* <Picker
-          selectedValue={time}
-          onValueChange={(itemValue, itemIndex) =>
-            setTime(itemValue)
-          }>
-          <Picker.Item label="15 minutes" value="15 minutes" />
-          <Picker.Item label="30 minutes" value="30 minutes" />
-        </Picker>  */}
-        <TextInput
-          style={styles.input}
-          onChangeText={setTime}
-          value={time}
-        />
-      </View>
 
-      {/* View for the Where Text input Form */}
-      <View >
-        <Text
-        >
-          Where: 
-        </Text>
-        {/* <Picker
-          selectedValue={location}
-          onValueChange={(itemValue, itemIndex) =>
-            setLocation(itemValue)
-          }>
-          <Picker.Item label="Scheller" value="Scheller" />
-          <Picker.Item label="CULC" value="CULC" />
-          <Picker.Item label="Exhibition Hall" value="Exhibition Hall" />
-          <Picker.Item label="Tech Square" value="Tech Square" />
-          <Picker.Item label="Tech Green" value="Tech Green" />
-        </Picker>  */}
-        <TextInput
-          style={styles.input}
-          onChangeText={setLocation}
-          value={location}
-        />       
-      </View>
+    
+    <View style={{padding:20}}>  
+    <Text>Search for Friend Below</Text>
 
-      <Button 
+      <SearchBar
+        placeholder="Search Here"
+        onSearchIcon={setTime}
+      />
+
+      {/* <Button 
           title='Submit'
           onPress={() => handlePress()}>      
-      </Button>
+      </Button> */}
     </View>
   )
 }
